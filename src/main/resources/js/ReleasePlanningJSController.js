8219520815982bad7f9c89946f093fdd9521c257
Alexander Perndorfer <alexander-perndorfer@hotmail.com>
2017-07-24 19:34:35 +0200

    Dialog

--- src/main/resources/js/ReleasePlanningJSController.js
+++ src/main/resources/js/ReleasePlanningJSController.js
@@ -1,9 +1,36 @@
-AJS.toInit(function(){
-   require(['aui/flag'], function(flag) {
-       var myFlag = flag({
-           type: 'success',
-           title: 'Release Planning Plugin',
-           body: 'JS Controller is working'
-       });
-   });
+$(function () {
+    $("#start-date").datepicker();
+    $("#end-date").datepicker();
+    $("#start-date").datepicker("option", "dateFormat", "dd.mm.yy");
+    $("#end-date").datepicker("option", "dateFormat", "dd.mm.yy");
+    var dialog = document.getElementById("dialog");
+    dialog.setAttribute("hidden",null);
+
+    $("#create-user").button().on("click", function () {
+
+        $("#dialog").dialog();
+
+        console.warn("click");
+        var blanket = document.getElementById("blanket");
+        blanket.removeAttribute("hidden");
+    });
+
 });
+
+function cancelClicked()
+{
+    console.warn("click");
+    $("#dialog").dialog("close");
+    var blanket = document.getElementById("blanket");
+    blanket.setAttribute("hidden", null);
+}
+
+function createProject()
+{
+    $("#dialog").dialog();
+    var dialog = document.getElementById("dialog");
+    dialog.removeAttribute("hidden");
+    console.warn("click");
+    var blanket = document.getElementById("blanket");
+    blanket.removeAttribute("hidden");
+}
