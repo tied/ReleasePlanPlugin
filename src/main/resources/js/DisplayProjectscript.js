bd2cee8120f027765b82258b6b95a46e3a871758
dberghammer <BerghammerDomi@gmail.com>
2017-08-24 19:07:38 +0200

    Anpassung Releaseplan table

--- src/main/resources/js/DisplayProjectscript.js
+++ src/main/resources/js/DisplayProjectscript.js
@@ -1,24 +1,31 @@
 
 window.onload = function addColumns() {
     var columncount = project.sprints; //get columns
-    document.getElementById("startSprint").innerHTML = '<p style="width: 100px">Sprint 1</p><br>  <input style="width: 100px"  id="sprint_end_date" title="Sprintenddate" onkeydown="return false" placeholder="DD.MM.YYYY" name="sprint_end_date" type="text"><br>    SP: <input style="width: 60px" type="number" value="' + project.storyPoints + '" ><br>';
+    document.getElementById("startSprint").innerHTML = '<p class="sprint_title">Sprint 1</p><br>\n\
+  <input style="width: 120px"  id="sprint_start_date" title="Sprintstartdate" onkeydown="return false" placeholder="Start Date" name="sprint_end_date" type="text"><br>\n\
+  <input style="width: 120px"  id="sprint_end_date" title="Sprintenddate" onkeydown="return false" placeholder="End Date" name="sprint_end_date" type="text"><br>\n\
+  <input style="width: 50px" type="number" value="' + 1 + '" >   \n\
+    * \n\
+  <input style="width: 50px" type="number" value="' + 1 + '" ><br>   \n\
+    SP: <input style="width: 90px" type="number" value="' + project.storyPoints + '" ><br>';
     for (var i = 1; i < columncount; i++) {
         var row = document.getElementById("rp_headers");
-        row.insertCell(i).innerHTML = '<td style="min-width:50px"><p style="width: 100px">Sprint ' +
-                 (i + 1) + '</p><br>  <input style="width: 100px"  id="sprint_end_date' + i +
-                '" title="Sprintenddate" onkeydown="return false" placeholder="DD.MM.YYYY" name="sprint_end_date" type="text"><br>   SP: <input style="width: 60px" type="number" value="' + project.storyPoints + '" ><br></td>';
+        row.insertCell(i).innerHTML = '<td style="min-width:50px"><p class="sprint_title">Sprint ' +(i + 1) + '</p><br> \n\
+        <input style="width: 120px"  id="sprint_start_date" title="Sprintstartdate" onkeydown="return false" placeholder="Start Date" name="sprint_end_date" type="text"><br>\n\
+        <input style="width: 120px"  id="sprint_end_date" title="Sprintenddate" onkeydown="return false" placeholder="End Date" name="sprint_end_date" type="text"><br>\n\
+        <input style="width: 50px" type="number" value="' + 1 + '" >   \n\
+        * \n\
+        <input style="width: 50px" type="number" value="' + 1 + '" ><br>   \n\
+        SP: <input style="width: 90px" type="number" value="' + project.storyPoints + '" ><br></td>';
 
-        $("#sprint_end_date"+i).datepicker({
+        $("#sprint_end_date" + i).datepicker({
             showOn: 'focus',
             showButtonPanel: true,
-            closeText: 'Clear', 
+            closeText: 'Clear',
             dateFormat: 'dd.mm.yy'
         });
 
         var row = document.getElementById("rp_row_2");
-        row.insertCell(i).innerHTML = '<td > </td>';
-
-        var row = document.getElementById("rp_row_3");
         row.insertCell(i).innerHTML = '<td > </td>';
 
 
@@ -32,15 +39,7 @@ window.onload = function addColumns() {
     $("#sprint_end_date").datepicker({
         showOn: 'focus',
         showButtonPanel: true,
-        closeText: 'Clear', 
+        closeText: 'Clear',
         dateFormat: 'dd.mm.yy'
     });
-
-
 }
-
-window.onresize = function(){
-    
-}
-
-
